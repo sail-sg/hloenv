53012d17ee4f353b1d653b52a16ea628bb3e4806
Oh Chin Yang <ohcy@sea.com>
2022-10-25 19:06:29 +0800

    Add an examples folder and a simple always_pick_first example + an example usage of the pipeline interface

---
+++ examples/always_pick_first.py
@@ -0,0 +1,74 @@
+# Copyright 2021 Garena Online Private Limited
+
+# A demo to where we always pick the first alternative and compare the
+# evaluation timing of the resulting hlo module to the one optimized by the 
+# original xla pipeline.
+
+import numpy as np
+import os
+from altgraph import AltPipeline, HloEnv, HloPass, Pass, Pipeline
+from general_fusion_pipeline import GeneralFusionPipeline
+
+if __name__ == '__main__':
+  os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+  base_dir = os.path.dirname(os.path.realpath(__file__))
+  hlo_base_dir = os.path.join(base_dir, "hlo_texts")
+  for root, dirs, files in os.walk(hlo_base_dir):
+    for file in files:
+
+      filepath = os.path.join(root, file)
+
+      hlo_env = HloEnv(filepath, "gpu")
+      general_fusion_pipeline = GeneralFusionPipeline(hlo_env)
+      instruction_count = hlo_env.get_hlo_module().instruction_count
+
+      print("-------------------------------------------------")
+      print("Testing general fusion for file: " + filepath)
+      print("    num instructions = %d" % instruction_count)
+
+      saved_hlo_module = hlo_env.save_hlo()
+      # Original TF pipelines
+      hlo_env.optimize_hlo_module()
+      hlo_env.prepare_hlo_module_for_ir_emitting()
+
+      ref_results = hlo_env.evaluate(100)
+      ref_timing = min(ref_results.durations)
+
+      # Save reference copy of the module after a non dry-run RunHloPasses call
+      reference_hlo_module = hlo_env.save_hlo()
+      hlo_env.load_hlo(saved_hlo_module)
+
+      hlo_env.run(general_fusion_pipeline.pre_pass_optimizations)
+      num_alts = 1
+      while num_alts > 0:
+        hlo_env.run(general_fusion_pipeline.pre_dry_pass_passes)
+        hlo_env.run(general_fusion_pipeline.pass_dry_run)
+
+        hlo_graph = hlo_env.get_hlo_graph(do_hash_verification=False)
+        node_features = hlo_graph.node_features
+        num_operands = node_features.num_operands
+        num_alts = len(hlo_graph.alternative_indices)
+
+        if num_alts > 0:
+          decisions = []
+          for alt_idx in hlo_graph.alternative_indices:
+            node_uid = node_features.uids[alt_idx]
+
+            # Always pick the first alternative available
+            decisions.append([node_uid, 1])
+
+          decisions = np.asarray(decisions)
+          hlo_env.apply_alternatives(decisions)
+
+          hlo_env.run(general_fusion_pipeline.post_dry_pass_passes)
+
+      hlo_env.run(general_fusion_pipeline.post_pass_optimizations)
+      hlo_env.prepare_hlo_module_for_ir_emitting()
+
+      pick_one_results = hlo_env.evaluate(100)
+      pick_one_timing = min(pick_one_results.durations)
+
+      print("Ref timing: %.2f, Pick one timing: %.2f" % 
+        (ref_timing, pick_one_timing))
